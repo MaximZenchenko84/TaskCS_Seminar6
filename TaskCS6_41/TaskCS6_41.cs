b3782f4bb2240faf372ff0c203a5e3d4f1f553d6
Maxim Zenchenko <nevelsk45@gmail.com>
2023-06-28 17:22:25 +0300

    Tasks for seminar 6 C#

---
+++ TaskCS6_41/TaskCS6_41.cs
@@ -0,0 +1,27 @@
+﻿// Урок 6. Задача 41. Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+using System;
+
+namespace TaskCS6_41
+{
+    class TaskCS6_41
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов массива, состоящего из вещественных чисел");
+            int num_elements = int.Parse(Console.ReadLine());
+            double[] array = new double[num_elements]; // объявление массива 
+            Random rnd = new Random();
+            int num_pol = 0; // счетчик чисел, больших нуля
+            // цикл ввода элементов массива и подсчета количества элементов, бОльших нуля
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Введите " + i + "-й элемент массива ");
+                array[i] = double.Parse(Console.ReadLine());
+                num_pol = (array[i] > 0) ? (num_pol+1) : num_pol;
+            }
+            Console.WriteLine("Количество элементов, бОльших нуля, равно " + num_pol);
+        }
+    }
+}
---
+++ TaskCS6_43/TaskCS_43.cs
@@ -0,0 +1,41 @@
+﻿//  Урок 6. Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1*x+b1, y = k2*x+b2;
+// значения b1, k1, b2 и k2 задаются пользователем
+
+using System;
+
+namespace TaskCS6_43
+{
+    class TaskCS_43
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите коэффициент наклона первой прямой k1 = ");
+            double k1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите свободное слагаемое первой прямой b1 = ");
+            double b1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите коэффициент наклона второй прямой k2 = ");
+            double k2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите свободное слагаемое второй прямой b2 = ");
+            double b2 = double.Parse(Console.ReadLine());
+            if (k1 == k2)
+            {
+                if (b1 == b2)
+                {
+                    Console.WriteLine("Прямые совпадают");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Прямые параллельны, точек пересечения нет");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Координаты точки пересечения (" + ((b2 - b1) / (k1 - k2)) + "; " + (k1 * (b2 - b1) / (k1 - k2) + b1) + ")");              
+            }
+
+        }
+    }
+}
